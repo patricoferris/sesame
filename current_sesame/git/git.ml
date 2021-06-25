8808693dcc96ab14cd027ebc4c16c924efb65709
Patrick Ferris <pf341@patricoferris.com>
2021-06-25 10:05:30 +0100

    fix git and add recursive dirs functor

--- current_sesame/git/git.ml
+++ current_sesame/git/git.ml
@@ -2,19 +2,18 @@ module type With_fs = Sesame.Types.S with type Input.t = Fpath.t
 
 module Make (FS : With_fs) = struct
   module Builder = struct
-    type t = Fpath.t -> Fpath.t
-
+    type t = No_context
     let auto_cancel = true
-
-    let pp = Current_git.Commit.pp
 
     let id = "current-sesame.git"
 
     module Key = struct
-      type t = Current_git.Commit.t
+      type t = Current_git.Commit.t * Fpath.t option
 
-      let digest = Current_git.Commit.marshal
+      let digest (c, f) = Fmt.str "%s-%a" (Current_git.Commit.marshal c) Fmt.(option Fpath.pp) f 
     end
+
+    let pp ppf (_, f) = Fmt.pf ppf "checkout with path %a" (Fmt.option Fpath.pp) f
 
     module Value = struct
       type t = FS.Output.t
@@ -24,17 +23,17 @@ module Make (FS : With_fs) = struct
       let unmarshal = FS.Output.decode
     end
 
-    let build f job repo =
+    let build No_context job (repo, rest) =
       let open Lwt.Infix in
       Current.Job.start job ~level:Current.Level.Harmless >>= fun () ->
-      Current_git.with_checkout ~job repo (fun path -> FS.build (f path))
+      Current_git.with_checkout ~job repo (fun path -> match rest with None -> FS.build path | Some rest -> FS.build (Fpath.(path // rest)))
   end
 
   module GC = Current_cache.Make (Builder)
 
-  let get ?schedule ?(label = "build with repo") f repo =
+  let get ?schedule ?(label = "build with repo") path repo =
     let open Current.Syntax in
     Current.component "%s" label
     |> let> repo = repo in
-       GC.get ?schedule f repo
+       GC.get ?schedule No_context (repo, path)
 end
